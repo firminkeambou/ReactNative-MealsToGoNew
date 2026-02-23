35b2f1af90d32dd06ce3026e624dbdafcbdde1ed
keamboufirmin <seven7digit2021@gmail.com>
2026-02-23 22:23:56 +1100

    meal to go up to chap 215 completed and firbase config file secured last

--- src/services/authentication/firebase/firebase.config.ts
+++ src/services/authentication/firebase/firebase.config.ts
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 // Initialize Firebase
 // Your web app's Firebase configuration/ remember we chose "web SDK(<>) from firebase console"
-/* const firebaseConfig = {
-    apiKey: "AIzaSyBbQU0Cliv5C01RM3ih2B7n060AzAOmd4Q",
-    authDomain: "mealstogoreactnativ.firebaseapp.com",
-    projectId: "mealstogoreactnativ",
-    storageBucket: "mealstogoreactnativ.firebasestorage.app",
-    messagingSenderId: "436530937554",
-    appId: "1:436530937554:web:477868f49047ca1f2ab70b",
-  }; */
+
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
