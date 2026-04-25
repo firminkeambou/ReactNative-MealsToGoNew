b4fa9ead7d93e8713109334cf447e392625a225e
firminkeambou <seven7digit2021@gmail.com>
2026-04-26 00:28:30 +0100

    dealing to build to build the app from windows, now returning back to MacOS

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,10 +1,10 @@
 // bear in mind that firebase functions works better in ios simulator as Android do not allow fetching data using "Local Host"
-import camelize from "camelize";
-import { hostLocation, liveHostLocation } from "../../utils/env";
+import camelize from 'camelize';
+import { hostLocation, liveHostLocation } from '../../utils/env';
 
 //import { locations } from '../../../functions/geocode/geocode.mock';
 // locationRequest() can be called by default in restaurants.screen.jsx in order to test out if it works
-export const locationRequest = async (searchTerm = "san francisco") => {
+export const locationRequest = async (searchTerm = 'san francisco') => {
   /* //default location == san francisco
   return new Promise((resolve, reject) => {
     const locationMock = locations[searchTerm];
@@ -33,23 +33,23 @@ export const locationRequest = async (searchTerm = "san francisco") => {
   try {
     //read carefully ${hostLocation} ; only work on simulator , not in real devices//error developpment
 
-    /* use this (${liveHostLocation}) if we want to test on real device 
+    //use this (${liveHostLocation}) if we want to test on real device
     const response = await fetch(`${liveHostLocation}?city=${searchTerm}`, {
-      method: "GET",
-    }); */
-    //use the following  with simulators
-    console.log("root string:", `${hostLocation}?city=${searchTerm}`);
-    const response = await fetch(`${hostLocation}?city=${searchTerm}`, {
-      method: "GET",
+      method: 'GET',
     });
+    //use the following  with simulators
+    console.log('root string:', `${hostLocation}?city=${searchTerm}`);
+    /* const response = await fetch(`${hostLocation}?city=${searchTerm}`, {
+      method: 'GET',
+    }); */
     const data = await response.json();
     console.log(
-      "Location request new from location.service.js/locationRequest function",
+      'Location request new from location.service.js/locationRequest function',
       data,
     );
     return locationTransform(data);
   } catch (error) {
-    console.error("Error fetching location data:", error, "free");
+    console.error('Error fetching location data:', error, 'free');
   }
   //locationTransform(locationMock)
 };
@@ -61,7 +61,7 @@ const locationTransform = (result) => {
   const viewport = geometry.viewport;
   const latLongString = `${lat},${lng}`; // to be used in restaurants request as location parameter takes a string "lat,lng"
   console.log(
-    "Location request new from location.service.js/restaurants.screen.jsx",
+    'Location request new from location.service.js/restaurants.screen.jsx',
     latLongString,
   );
   return { latLongString, viewport, lat, lng };
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,28 +1,28 @@
 // bear in mind that firebase functions works better in ios simulators as Android do not allow fetching data using "Local Host"
 
-import camelize from "camelize";
-import { hostPlaces, liveHostPlaces } from "../../utils/env";
+import camelize from 'camelize';
+import { hostPlaces, liveHostPlaces } from '../../utils/env';
 export const restaurantsRequest = async (latitude, longitude) => {
   //location = '37.7749295,-122.4194155'
   //default location == san francisco
   //
   try {
     //${hostPlaces} only works for simulators //error developpment
-    /* this line should be use to test on real devices
+    //this line should be use to test on real devices,deploying on line
     const response = await fetch(
       `${liveHostPlaces}?lat=${latitude}&long=${longitude}`,
       {
-        method: "GET",
+        method: 'GET',
       },
     );
-    */
+
     // use the below line for simulators and tests environnment
-    const response = await fetch(
+    /* const response = await fetch(
       `${hostPlaces}?lat=${latitude}&long=${longitude}`,
       {
         method: "GET",
       },
-    );
+    ); */
     const data = await response.json();
     /* console.log(
       "Location request new from restaurants.service.js/restaurantsRequest function",
@@ -30,7 +30,7 @@ export const restaurantsRequest = async (latitude, longitude) => {
     ); */
     return restaurantsTransform(data);
   } catch (error) {
-    console.error("Error fetching restaurants places:", error);
+    console.error('Error fetching restaurants places:', error);
   }
   /////;;;;;;;;;;;;;;;;;;
   /* return new Promise((resolve, reject) => {
@@ -61,7 +61,7 @@ const restaurantsTransform = ({ results = [] }) => {
     return {
       ...restaurant,
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
-      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+      isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARILY',
       address: restaurant.vicinity,
     };
   });
